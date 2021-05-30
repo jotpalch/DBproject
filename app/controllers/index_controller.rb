492d537bb09d2956c186eb64e362931e31b6088e
jotpalch <jotp076315217@gmail.com>
2021-05-31 05:27:26 +0800

    fix page to #search

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,21 @@
 class IndexController < ApplicationController
+
   def index
+      p(params)
+      p(request.url)
+      p(request.env['HTTP_REFERER'])
+      p(request.query_string)
       @bikestole = BikeStole
         .all
         .page(params[:page])
+      # if(params[:p]!="1")
+      #   redirect_to("/?page="+params[:page]+"&p=1#search")
+      # end
+
   end
+
+  def test
+    redirect_to("/?page="+params[:page]+"#search")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'index#index'
   get '/index' , to: 'index#index'
+  get '/:page' => "index#test"
+
 end
