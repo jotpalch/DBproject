7808bf042253a535e903fbe4d34dbac2741eabdc
jotpalch <jotp076315217@gmail.com>
2021-05-31 05:40:46 +0800

    find a new method to add anchor

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,17 +1,9 @@
 class IndexController < ApplicationController
 
   def index
-      p(params)
-      p(request.url)
-      p(request.env['HTTP_REFERER'])
-      p(request.query_string)
       @bikestole = BikeStole
         .all
         .page(params[:page])
-      # if(params[:p]!="1")
-      #   redirect_to("/?page="+params[:page]+"&p=1#search")
-      # end
-
   end
 
   def test
