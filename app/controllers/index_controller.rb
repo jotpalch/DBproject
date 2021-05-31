39095b37be4af17a4902a3e4052cbbf75cdb7e8e
jotpalch <jotp076315217@gmail.com>
2021-06-01 06:04:32 +0800

    Search bar and display

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,13 +1,28 @@
 class IndexController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def index
-      @bikestole = BikeStole
-        .all
-        .page(params[:page])
+      @bikestoles = BikeStole.all
+
+  end
+
+  def search
+    p(params)
+    @data = case params[:type]
+      when "1" then BikeStole.all
+      when "2" then ScooterStole.all
+      when "3" then CarStole.all
+      when "4" then HomeStole.all
+      #else BikeStole.all+ScooterStole.all+CarStole.all+HomeStole.all
+    end
+    @data = @data
+    .page(params[:page])
   end
 
   def test
-    redirect_to("/?page="+params[:page]+"#search")
+    @bikestole = BikeStole.all
+    p(params)
+    render partial: "test"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'index#index'
   get '/index' , to: 'index#index'
-  get '/:page' => "index#test"
+  get '/search' => 'index#search'
+
+  get '/test' => "index#test"
+
+  resources :bike_stoles
+  post '/search' => "index#search"
 
 end
