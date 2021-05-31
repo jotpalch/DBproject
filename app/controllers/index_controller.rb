8e4996378f3d6495616767c7829951d391b1f16f
jotpalch <jotp076315217@gmail.com>
2021-06-01 07:43:48 +0800

    search function almost complete

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -7,16 +7,20 @@ class IndexController < ApplicationController
   end
 
   def search
-    p(params)
+    p(params[:year])
+    date_search = (params[:year].to_i-1911).to_s+format("%02d",params[:month]).to_s
+    p(date_search)
     @data = case params[:type]
-      when "1" then BikeStole.all
-      when "2" then ScooterStole.all
-      when "3" then CarStole.all
-      when "4" then HomeStole.all
+      when "自行車竊盜" then BikeStole.all
+      when "機車竊盜" then ScooterStole.all
+      when "汽車竊盜" then CarStole.all
+      when "住宅竊盜" then HomeStole.all
       #else BikeStole.all+ScooterStole.all+CarStole.all+HomeStole.all
     end
     @data = @data
+    .where("location LIKE '%#{params[:district]}%' AND date LIKE '#{date_search}%'")
     .page(params[:page])
+    p(@data==[])
   end
 
   def test
