2e1ca6039254a99698b8f3861791e932da4d6d2d
jotpalch <jotp076315217@gmail.com>
2021-06-01 09:37:35 +0800

    perfect the search

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,14 +2,16 @@ class IndexController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-      @bikestoles = BikeStole.all
 
   end
 
   def search
-    p(params[:year])
-    date_search = (params[:year].to_i-1911).to_s+format("%02d",params[:month]).to_s
-    p(date_search)
+    if params[:month]!="整年"
+      params[:month] = "_"+format("%02d",params[:month]).to_s
+      date_search = (params[:year].to_i-1911).to_s+params[:month][1..2]
+    else
+      date_search = (params[:year].to_i-1911).to_s
+    end
     @data = case params[:type]
       when "自行車竊盜" then BikeStole.all
       when "機車竊盜" then ScooterStole.all
@@ -17,10 +19,14 @@ class IndexController < ApplicationController
       when "住宅竊盜" then HomeStole.all
       #else BikeStole.all+ScooterStole.all+CarStole.all+HomeStole.all
     end
+    if params[:district] != ""
+      @data = @data.where("location LIKE '%#{params[:district]}%'")
+    end
     @data = @data
-    .where("location LIKE '%#{params[:district]}%' AND date LIKE '#{date_search}%'")
-    .page(params[:page])
-    p(@data==[])
+    .where("date LIKE '#{date_search}%'")
+    .order(:date)
+    @count = @data.count()
+    @data = @data.page(params[:page])
   end
 
   def test
