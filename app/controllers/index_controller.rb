2a083f0fbae278b1024855a8989c07e88064390a
jotpalch <jotp076315217@gmail.com>
2021-06-03 20:36:57 +0800

    cctv image

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -13,6 +13,11 @@ class IndexController < ApplicationController
         marker.lat results.first.coordinates[0]
         marker.lng results.first.coordinates[1]
         marker.infowindow ps.name
+        marker.picture({
+                     :url =>"/images/cctv_w.png",
+                     :width   => 40,
+                     :height  => 40
+                 })
     end
     # @hash = [{:lat=>25.03368625, :lng=>121.56481033101315, :infowindow=>"1"}, {:lat=>25.0375198, :lng=>121.5636796, :infowindow=>"123"}, {:lat=>23.3332726, :lng=>121.3162746, :infowindow=>"111"}]
   end
